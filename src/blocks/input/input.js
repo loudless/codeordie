d403b6d48f261f920c3295eaedae89f423e3e244
Litvinov Alexander <alexander@codeordie.ru>
2014-10-08 23:47:23 +0400

    Удалил .DS_Store. Отрефакторил сохранение email.

--- src/blocks/input/input.js
+++ src/blocks/input/input.js
@@ -1,18 +1,51 @@
 $.widget('codeordie.input', {
 
-    _create: function () {
+    _config: {
+        domain: 'api.codeordie.ru',
+        ssl: false,
+        type: 'POST',
+        method: 'promo/save'
+    },
 
+    _create: function () {
         this._initEvents();
+        this.$email = $(this.element)
     },
 
     _initEvents: function () {
         this._on({
-            'keypress': this._publishEmail
+
         });
 
+        $.subscribe('save', $.proxy(this._save, this));
     },
 
-    _publishEmail: function () {
-        $.publish('email', [this.element.val()]);
+    _save: function (e, data) {
+        console.log('Hello');
+        var config = this._config;
+        var url = config.ssl ? 'https://' + config.domain : 'http://' + config.domain + '/' + config.method;
+        if (this._validate()) {
+            $.ajax({
+                type: config.type,
+                url: url,
+                cache: false,
+                crossDomain: true,
+                data: {email: this.$email.val()},
+                success: this._success
+            });
+        }
+    },
+
+    _success: function (result  ) {
+        console.log(result);
+    },
+
+    /**
+     * @todo Добавить проверку на email
+     * @private
+     */
+    _validate: function () {
+        return true;
     }
+
 });
