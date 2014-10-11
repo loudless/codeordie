63039e482db0bd2b872833d463bb402fb7e7e44c
loudless <john.loudless@gmail.com>
2014-10-12 02:03:41 +0400

    parallax mouse position fix

--- src/blocks/back/back.js
+++ src/blocks/back/back.js
@@ -21,47 +21,18 @@ $.widget('codeordie.back', {
 				var mouseXPercent = Math.round(xPos / $(this).width() * 100);
 				var mouseYPercent = Math.round(yPos / $(this).height() * 100);
 
-				console.log(mouseXPercent/20);
+				console.log((mouseXPercent-50)/20);
 
 				self.element.css({
 					'background-position': 
-						(50 + mouseXPercent/20) + '% 50%,' +
-						(50 + mouseXPercent/20) + '% 50%,' +
-						(50 - mouseXPercent/20) + '% 50%,' +
-						(50 - mouseXPercent/40) + '% 50%,' +
-						(50 - mouseXPercent/40) + '% 50%,' +
+						(50 + (mouseXPercent-50)/20) + '% 50%,' +
+						(50 + (mouseXPercent-50)/20) + '% 50%,' +
+						(50 - (mouseXPercent-50)/20) + '% 50%,' +
+						(50 - (mouseXPercent-50)/40) + '% 50%,' +
+						(50 - (mouseXPercent-50)/40) + '% 50%,' +
 						 '50% 50%, ' + 
-						(50 - mouseXPercent/20) + '% 50%'
-				})
-
-				/* Position Each Layer */
-				// $(this).children('img').each(
-				// 	function() {
-				// 		var diffX = $('#Parallax').width() - $(this).width();
-				// 		var diffY = $('#Parallax').height() - $(this).height();
-
-				// 		var myX = diffX * (mouseXPercent / 100); //) / 100) / 2;
-
-
-				// 		var myY = diffY * (mouseYPercent / 100);
-
-
-				// 		var cssObj = {
-				// 				'left': myX + 'px',
-				// 				'top': myY + 'px'
-				// 			}
-				// 			//$(this).css(cssObj);
-				// 		$(this).animate({
-				// 			left: myX,
-				// 			top: myY
-				// 		}, {
-				// 			duration: 50,
-				// 			queue: false,
-				// 			easing: 'linear'
-				// 		});
-
-				// 	}
-				// );
+						(50 - (mouseXPercent-50)/20) + '% 50%'
+				});
 
 			}
 		);
